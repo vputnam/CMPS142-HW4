e0b4078660e47e26128ead138a2de36dce32a001
vputnam <vputnam@ucsc.edu>
2017-03-18 18:59:07 -0700

    Add files via upload

---
+++ a4q1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import tensorflow as tf
+
+###############################################################################
+# Create the tensor x of the value 
+#   [[[2, -2, 4], [0, 1, 2]], [[1, 2, 3], [7, 1, 9]]] 
+# and y as a tensor of ones with the same shape as x.
+# Return a Boolean tensor that yields Trues with the same shape as x and y if 
+# x equals y element-wise.
+# Hint: Look up tf.equal().
+###############################################################################
+def p1():
+    x = tf.constant([[[2, -2, 4], [0, 1, 2]], [[1, 2, 3], [7, 1, 9]]])
+    y = tf.ones_like(x)
+    z = tf.equal(x, y)
+    return z
+
+################################################################################
+# Creates one variable 'x' of the value [3., -4.] and a placeholder 'y' of the 
+# same shape as 'x'. Given a scalar z returns a triple containing 
+#   x
+#   y 
+#   and a TensorFlow object that returns x + y if z > 0, and x - y otherwise. 
+# Hint: Look up tf.cond().
+################################################################################
+def p2(z):
+    x = tf.Variable([3., -4.])
+    y = tf.placeholder(tf.float32,shape= (1,2)) #x.get_shape())
+    result = tf.cond(tf.greater(z,0), lambda: tf.add(x,y), lambda: tf.sub(x,y))
+    obj = [x,y,result]
+    return obj
+
+###############################################################################
+# Given 2d tensors x, y, and z, returns a tensor object for  x' * y^-1 + z 
+# where x' is the transpose of x and y^-1 is the inverse of y. The dimensions 
+# of tensors will be compatible.
+# Hint: See "Matrix Math Functions" in TensorFlow documentation.
+###############################################################################
+def p3(x, y, z):
+    y_inv = tf.matrix_inverse(y)
+    x_trans = tf.transpose(x)
+    mul = tf.matmul(x_trans, y_inv)
+    result = tf.add(mul,z)
+    return result
+
+###############################################################################
+# Given a TensorFlow object that describes a convex function and a TensorFlow 
+# session, return the the minimum value of the function found by gradient 
+# decent. Use 0.01 learning rate and 10000 steps. 
+###############################################################################
+def p4(objective_function, sess):
+    learning_rate = .01
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train = optimizer.minimize(objective_function)
+    for _ in xrange(10000):
+        sess.run(train)
+    return sess.run(objective_function)
+
+
+
+
---
+++ a4q2.py
@@ -0,0 +1,44 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import tensorflow as tf
+import numpy as np
+from tensorflow.contrib import learn
+
+
+def LR():
+    tr,te  = [],[]
+    with open('a4q2_train.txt', 'r') as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        line = map(float, line.split())
+        tr.append(line)
+    train = np.array(tr)
+
+    trX = train[:, [0,1]]
+    trY = train[:,[2]]
+
+    with open('a4q2_test.txt', 'r') as g:
+        lines = g.read().splitlines()
+    for line in lines:
+        line = map(float, line.split())
+        te.append(line)
+    test = np.array(te)
+
+    teX = test[:, [0,1]]
+    teY = test[:,[2]]
+
+    features = [tf.contrib.layers.real_valued_column("x1"), tf.contrib.layers.real_valued_column("x2")]
+    estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
+    input_fn = tf.contrib.learn.io.numpy_input_fn({"x1":trX1, "x2":trX2}, trY, batch_size = 4, num_epochs = 1000)
+    testing_in = tf.contrib.learn.io.numpy_input_fn({"x1": teX1, "x2":teX2}, teY, batch_size=4, num_epochs = 1000)
+    print(estimator.fit(input_fn=input_fn, steps=1000))
+    print(estimator.evaluate(input_fn=testing_in))
+
+    return ""
+
+
